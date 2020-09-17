d6599fe3b02da6c0ae0f7e6fcc49092efbc60910
Robertas <r.ankudovicius@gmail.com>
2020-09-17 09:48:38 +0300

    added api call

---
+++ components/prices.js
@@ -0,0 +1,8 @@
+const Prices = ({ bpi }) => (
+  <>
+    <ul className='list-group'>
+      <li className='list-group-item'>Bitcoin rate for</li>
+    </ul>
+  </>
+);
+export default Prices;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Layout from '../components/layout';
+import Prices from '../components/Prices';
 
-export default () => (
+const Index = ({ bpi }) => (
   <>
-    <Layout>Hello</Layout>
+    <Layout>
+      Hello
+      <Prices usd={bpi.USD} />
+    </Layout>
   </>
 );
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+
+  return { bpi: data.bpi };
+};
+
+export default Index;
